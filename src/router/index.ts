b6adecce84c3fd2fe99025a7188f4fe26d11cf57
yyd <609825025@qq.com>
2021-04-14 18:16:47 +0800

    修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,19 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
-const routes: Array < RouteRecordRaw > = [{
+const routes: RouteRecordRaw[] = [{
         path: '/',
         redirect: '/home',
     },
     {
+        path: '/test',
+        component: () => import('@/views/test/test.vue')
+    },
+    {
         path: '/home',
         component: () => import('@/views/home/home.vue')
-    }
+    },
 ];
 export default createRouter({
     history: createWebHistory(),
-    routes
+    routes,
 });
