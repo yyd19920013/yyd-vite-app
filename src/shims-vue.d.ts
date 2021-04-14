33f8d50faf0f88cbf6e3c5241d49929ad16a169b
yyd <609825025@qq.com>
2021-04-14 17:46:09 +0800

    修改

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,14 +1,10 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import { Route } from 'vue-router';
-// import { Store } from 'vuex';
+import { Router } from 'vue-router';
+import { Store } from 'vuex';
 
 // 扩充
 declare module 'vue/types/vue' {
   interface Vue {
-    $router: VueRouter;
-    $route: Route;
-    // $store: Store<any>;
-    $api: any;
+    $router: Router;
+    $store: Store<any>;
   }
 }
